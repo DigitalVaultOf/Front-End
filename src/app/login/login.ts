82d688a27d02bca361fda1a04bd7b7675f4603b9
Carlos Cimitan <eduardo6cimitan@gmail.com>
2025-07-02 12:18:31 -0300

    Tela login

---
+++ src/app/login/login.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../services/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule,FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  accountNumber = '';
+  password = '';
+
+  constructor(
+    private http: HttpClient,
+    private auth: Auth,
+    private router: Router
+  ){}
+
+  login() {
+    this.http
+      .post<any>('https://localhost:7178/auth/api/Auth/login', {
+        accountNumber: this.accountNumber,
+        password: this.password,
+      })
+      .subscribe({
+        next: (res) => {
+          this.auth.login(res.data.token);
+          this.router.navigate(['/']);
+        },
+        error: () => alert('Falha no login.'),
+      });
+  }
+}
