73b8753f2e2179885ff6314e64d74d15580e5c3f
moisescaris <moises.caris@edu.unifil.br>
2025-07-02 11:20:21 -0300

    SPA configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Deposito } from './deposito/deposito';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: App},
+  { path: 'deposit', component: Deposito },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected title = 'Front-End-Net';
+  showContent = true;
+
+  constructor(private router: Router) {}
+
+  goTo(path: string) {
+    this.showContent = false;
+    this.router.navigate([path]);
+  }
 }
