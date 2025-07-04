8c7a8d249a781a2aa3f40a8ac6ba182a196758a7
CarlosCimitan <eduardo6cimitan@gmail.com>
2025-07-04 14:46:36 -0300

    deposit

--- src/app/deposito/deposito.ts
+++ src/app/deposito/deposito.ts
@@ -1,20 +1,35 @@
 import { OverlayRef } from '@angular/cdk/overlay';
 import { Component, Inject, Input } from '@angular/core';
+import { Deposit } from '../services/deposit';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-deposito',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './deposito.html',
   styleUrl: './deposito.scss'
 })
 export class Deposito {
   @Input() deposito: any;
+  valorDeposito: number = 0;
 
-  constructor(@Inject(OverlayRef) private overlayRef: OverlayRef) {}
+  constructor(@Inject(OverlayRef) private overlayRef: OverlayRef, private depositService: Deposit) {}
 
   closeModal() {
   console.log('Fechando modal...');
   this.overlayRef.dispose();
+  }
+
+  confirmarDeposito(){
+    this.depositService.deposit(this.valorDeposito).subscribe({
+      next: (res) => {
+        console.log('deposito feito com sucesso');
+        this.closeModal();
+      },
+      error: (err) => {
+        console.log('erro ao fazer deposito: ',err)
+      }
+    })
   }
 }
