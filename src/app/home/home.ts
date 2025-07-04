ca1cd5cf827dd6c465d8635541562ca77140f377
DaniloCossioloDias <danilocossiolodiass@gmail.com>
2025-07-04 10:54:09 -0300

    fix: modal

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Injector, Type } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { Transferencia } from '../transferencia/transferencia';
+import { Deposito } from '../deposito/deposito';
+import { Withdraw } from '../withdraw/withdraw';
+import { Overlay, OverlayModule, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal, PortalModule } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, RouterOutlet, OverlayModule, PortalModule],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
@@ -14,10 +19,45 @@ export class Home {
   protected title = 'Front-End-Net';
   showContent = true;
 
-  constructor(private router: Router) {}
+  private overlayRef?: OverlayRef;
+
+  Transferencia = Transferencia;
+  Deposito = Deposito;
+  Withdraw = Withdraw;
+  
+  constructor(private overlay: Overlay, private router: Router) {}
 
   goTo(path: string) {
     this.showContent = false;
     this.router.navigate(["/home/",path]);
+  }
+
+  openModal(component: Type<any>) {
+    this.overlayRef?.dispose();
+
+    this.overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      positionStrategy: this.overlay.position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+    });
+
+    const injector = this.createInjector(this.overlayRef);
+    const portal = new ComponentPortal(component, null, injector);
+    this.overlayRef.attach(portal);
+
+    this.overlayRef.backdropClick().subscribe(() => this.closeModal());
+  }
+
+  closeModal() {
+    this.overlayRef?.dispose();
+    this.overlayRef = undefined;
+  }
+
+  private createInjector(overlayRef: OverlayRef): Injector {
+    return Injector.create({
+      providers: [{ provide: OverlayRef, useValue: overlayRef }],
+    });
   }
 }
