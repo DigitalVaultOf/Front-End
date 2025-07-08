9d7eb88a042ec2538c4f691939d8552e4a1c1e24
CarlosCimitan <eduardo6cimitan@gmail.com>
2025-07-08 15:46:59 -0300

    Confirmaçao com senha

--- src/app/deposito/deposito.ts
+++ src/app/deposito/deposito.ts
@@ -2,34 +2,68 @@ import { OverlayRef } from '@angular/cdk/overlay';
 import { Component, Inject, Input } from '@angular/core';
 import { Deposit } from '../services/deposit';
 import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-deposito',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './deposito.html',
   styleUrl: './deposito.scss'
 })
 export class Deposito {
   @Input() deposito: any;
   valorDeposito: number = 0;
+  senha: string = '';
+  mensagemErro: string | null = null;
+  exibirSenha: boolean = false;
 
-  constructor(@Inject(OverlayRef) private overlayRef: OverlayRef, private depositService: Deposit) {}
+
+  constructor(@Inject(OverlayRef) private overlayRef: OverlayRef, private depositService: Deposit,  private cdr: ChangeDetectorRef) {}
 
   closeModal() {
   console.log('Fechando modal...');
   this.overlayRef.dispose();
   }
 
-  confirmarDeposito(){
-    this.depositService.deposit(this.valorDeposito).subscribe({
-      next: (res) => {
-        console.log('deposito feito com sucesso');
-        this.closeModal();
+  confirmarDeposito() {
+    console.log('ConfirmarDeposito acionado');
+    
+    if (!this.exibirSenha) {
+      console.log('Exibindo campo de senha...');
+      this.exibirSenha = true;
+  
+      this.cdr.detectChanges();
+  
+      setTimeout(() => {
+        this.cdr.detectChanges();
+      }, 0);
+  
+      return;
+    }
+  
+    const dto = {
+      value: this.valorDeposito,
+      password: this.senha
+    };
+  
+    this.depositService.deposit(dto).subscribe({
+      next: (res: any) => {
+        if (res?.data) {
+          this.mensagemErro = null;
+          this.closeModal();
+        } else {
+          this.mensagemErro = res?.message || 'Algo deu errado.';
+        }
       },
       error: (err) => {
-        console.log('erro ao fazer deposito: ',err)
+        this.mensagemErro = err.error?.message || 'Erro ao fazer depósito.';
       }
-    })
+    });
   }
+  
+  
+  
+  
 }
